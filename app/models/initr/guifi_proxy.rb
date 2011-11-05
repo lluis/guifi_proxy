162eb15b6d1f71c6415eb2c7e9a80fb4ebd0170f
Lluís <lluis@ingent.net>
2011-11-05 01:32:01 +0100

    millorar la gestio de squid.conf

--- app/models/initr/guifi_proxy.rb
+++ app/models/initr/guifi_proxy.rb
@@ -1,16 +1,17 @@
 class Initr::GuifiProxy < Initr::Klass
   unloadable
-  self.accessors_for(%w( guifi_base_url guifi_node_number guifi_squid_realm guifi_squid_port guifi_squid_lan ))
+  self.accessors_for(%w( guifi_base_url guifi_node_number guifi_squid_realm guifi_squid_port guifi_squid_lan guifi_squid_logformat ))
   validates_presence_of :guifi_node_number, :guifi_squid_realm, :guifi_squid_port, :on => :update
   validates_format_of :guifi_squid_lan, :with => /^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{2}$/, :allow_blank => true
   validates_format_of :guifi_squid_port, :with => /^[0-9]+$/
 
   def initialize(attributes=nil)
     super
-    self.guifi_base_url    ||= "http://www.guifi.net"
-    self.guifi_squid_realm ||= "Proxy federat de guifi.net"
-    self.guifi_squid_port  ||= "3128"
-    self.guifi_squid_lan   ||= ""
+    self.guifi_base_url        ||= "http://www.guifi.net"
+    self.guifi_squid_realm     ||= "Proxy federat de guifi.net"
+    self.guifi_squid_port      ||= "3128"
+    self.guifi_squid_lan       ||= ""
+    self.guifi_squid_logformat ||= "squid"
   end
 
 end
